dce6b7b074f10ed9d39f449df7d8ae35ac460c07
sevenOS <sevenos@sevenOSs-Mac.local>
2017-06-20 01:48:03 +0800

    Hv4

--- seveS04/main.swift
+++ seveS04/main.swift
@@ -37,26 +37,77 @@ import Foundation
 
 //if3Prac()
 
-//*********
-//var dice = Array.init(repeatElement(0, count: 6))  //[0,0,0,0,0,0]  => Array<Int>
-//
-//print(dice.description)
-//
-////var rand:UInt32   ///func 1
-//
-//var rand:Int   ///func 1
-//
-//for _ in 1...100 {
-//    rand = Int(arc4random_uniform(9))  //0 ... 5
-//    
-//    rand = rand >= 6 ? rand - 3 : rand   //cheat
-//    
-////rand = arc4random_uniform(6)  //func 1 0 ... 5
-//    
-//    dice[rand] += 1
-////    dice[Int(rand)] += 1   //func 1
-//    
-//}
-//for (i, value) in dice.enumerated() {
-//    print("\(i+1)點出現\(value)次")
-//}
+
+
+
+
+
+//****** myarray1 
+
+//myarray1()
+
+//emp()
+
+//capa()
+
+//myarray2()
+
+//enu()
+
+//enuPra()
+
+
+//********* 擲色子
+var dice = Array.init(repeatElement(0, count: 6))  //[0,0,0,0,0,0]  => Array<Int>
+
+print(dice.description)
+
+//var rand:UInt32   ///func 1
+
+var rand:Int   ///func 1
+
+for _ in 1...100 {
+    rand = Int(arc4random_uniform(9))  //0 ... 9
+    
+    rand = rand >= 6 ? rand - 3 : rand   //cheat  三元計算式
+    
+    //==> if rand >= 6 {rand - 3} else {rand = rand}
+    
+    
+//func2
+//rand = arc4random_uniform(6)  // 0 ... 5
+    
+    dice[rand] += 1
+//    dice[Int(rand)] += 1   //func 1
+//    print(dice.description)
+}
+for (i, value) in dice.enumerated() {
+    print("\(i+1)點出現\(value)次")
+    
+    
+    
+}
+
+print("=====")
+
+for value in dice {
+
+print(value)
+
+}
+
+print("=====")
+
+
+for i in 0..<dice.count {
+print(dice[i])
+
+    
+}
+print("=====")
+
+for (i,value) in dice.enumerated() {
+        print("\(i+1):\(value)")
+
+}
+
--- seveS04/myarray1.swift
+++ seveS04/myarray1.swift
@@ -1,9 +1,11 @@
 import Foundation
 
-func myarray(){
+
+//array 型別
+func myarray1(){
 
     
-    var a7 = [1, 2 ,3, 4]  // Areat<Int> =>泛型
+    var a7 = [1, 2 ,3, 4]  // Array<Int> =>泛型
     
     print(type(of:a7))
     print(a7[2])
@@ -12,39 +14,86 @@ func myarray(){
     print(type(of:a2))
     print(a2[2])
     
+    
+    
+    
     var a3:[Int] = []
-    let a4:Array<Int> = Array()
+    
+       let a4:Array<Int> = Array()
+    
+    
+    
+ 
+    
+    
     let a5:[Int] = Array()
-    let a6:Array<Int> = []
+    let _:Array<Int> = []
     let a8:[Int]?
+    let a9:Array<Int> = Array()
+    let a10:[Int] = []
     
-    
+    print(a10)
+}
+
+
+
+
+//**** isEmpty
+
+func emp(){
+    var a7:Array<Int> = []   //空陣列及為empty
     if a7.isEmpty {
         print("empty")
     }else{
         print("contents")
     }
     
+    var a8:[Int] = [4]
+    if a8.isEmpty {
+        print("empty")
+    }else{
+        print("contents")
+    }
+}
+
+
+
+//******* Capacity
+
+func capa(){
+
+
+    var a7:[Int] = []
+    var a3:Array<Int> = [2,3,4,5,8,6]
     
     a7.reserveCapacity(100)  //給他容量100
     print(a7.count)  //數目前的元素數量
     print(a3.count)
-    print(a7.capacity)  //容量  目前四個元素  跟大小不一樣
+    print(a7.capacity)  //容量    跟大小不一樣
     
     let a9 = Array(repeating: 0, count: 6)
     print(a9.description) // 將變數內容轉字串 a9.toString() JAVA用法
+
+    let a10 = Array(repeating: 123, count: 10)
+    print(a10.description) //print string
+    print(a10)   //print array
+
+}
+
+
+func myarray2(){
+
+    var a3: [Int] = []
+
+    //a3[0] = 1  //不能這樣  因原陣列為空的 連index都沒有
     
-    
-    
-    //a3[0] = 1  //不能這樣  空的 連index都沒有
-    
-    a3.append(123)
+    a3.append(123)  //append just one element
     print(a3.description)
     
-    a3 += [321]
+    a3 += [321,456,789]  // += can more than one element
     print(a3.description)
     
-    a3.append(contentsOf: [10,11,12])  //contentsof版本
+    a3.append(contentsOf: [10,11,12])  //contentsof版本 can add more than one
     print(a3.description)
     
     a3[1] = 77  //置換的意思 不能指定不存在的元素
@@ -58,17 +107,30 @@ func myarray(){
     a3.remove(at: 3)
     print(a3.description)
     
+//    a3.insert(444,at: 28)  //不能insert 在不存在的index
     
     
-    //元素反轉 reverse
+    }
+
+    //*******元素反轉 reverse?????
+
+
+
+
+    //置換 replace 某一段   replaceSubrange????
     
-    //置換 replace 某一段   replaceSubrange
     
     
+  //*****印出陣列
+    func enu(){
+    
+    
+  var  a3:[Int] = [123,456,456,888,999,111,222,555]
     for value in a3{
         print(value)
     }
-    
+    print("----------")
+
     for i in 0..<a3.count {
         print(a3[i])
     }
@@ -81,6 +143,36 @@ func myarray(){
     
     
 
+    
+}
+
+
+
+func enuPra(){
+
+    var  a4:[Int] = [123,456,456,888,999,111,222,555]
+
+   //foreach
+    for value in a4 {
+    
+    print(value)
+    }
+    
+    print("=====")
+    // for
+    for i in 0..<a4.count {
+    print(a4[i])
+    
+    }
+    print("=====")
+    // foreach 2 
+    
+    for (index,value) in a4.enumerated() {
+        print("\(index):\(value)")
+    
+    }
+    
+    
     
 }
 
